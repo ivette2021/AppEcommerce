2c8be73b6a69d1996384c19dc94b292c620f3ec2
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-16 01:13:36 -0300

    - habilitar view binding - diseño boton navigation view - definir dimensiones

--- app/src/main/java/com/ivetteVG/ecommerce/MainActivity.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ivetteVG.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ivetteVG.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
