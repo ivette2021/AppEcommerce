06e11b11b678447067d215290c1269e4e9f8cc39
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-17 16:15:44 -0300

    - logica del registro de usuario

--- app/src/main/java/com/ivetteVG/ecommerce/Opciones_Login/Login_email.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/Opciones_Login/Login_email.kt
@@ -8,11 +8,16 @@ import com.ivetteVG.ecommerce.Registro_email
 import com.ivetteVG.ecommerce.databinding.ActivityLoginEmailBinding
 
 class Login_email : AppCompatActivity() {
+
+
     private lateinit var binding :ActivityLoginEmailBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.TxtRegistrarme.setOnClickListener {
             startActivity(Intent(this@Login_email,Registro_email::class.java))
         }
