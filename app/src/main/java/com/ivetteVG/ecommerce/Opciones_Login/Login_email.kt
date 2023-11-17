26f8d3289d8475ada705ec3e4bc7bc19f472ecf4
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-17 00:49:06 -0300

    - diseño del registro de usuario con email

--- app/src/main/java/com/ivetteVG/ecommerce/Opciones_Login/Login_email.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/Opciones_Login/Login_email.kt
@@ -1,12 +1,20 @@
 package com.ivetteVG.ecommerce.Opciones_Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ivetteVG.ecommerce.R
+import com.ivetteVG.ecommerce.Registro_email
+import com.ivetteVG.ecommerce.databinding.ActivityLoginEmailBinding
 
 class Login_email : AppCompatActivity() {
+    private lateinit var binding :ActivityLoginEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_email)
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.TxtRegistrarme.setOnClickListener {
+            startActivity(Intent(this@Login_email,Registro_email::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/ivetteVG/ecommerce/Registro_email.kt
@@ -0,0 +1,11 @@
+package com.ivetteVG.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_email)
+    }
+}
