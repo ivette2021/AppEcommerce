bc1a6e5c3f7f36b3f61b355f11533d2b70e867d3
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-17 00:16:19 -0300

    - diseño de login

--- app/src/main/java/com/ivetteVG/ecommerce/OpcionesLogin.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/OpcionesLogin.kt
@@ -1,17 +1,26 @@
 package com.ivetteVG.ecommerce
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.ivetteVG.ecommerce.Opciones_Login.Login_email
+import com.ivetteVG.ecommerce.databinding.ActivityOpcionesLoginBinding
 
 class OpcionesLogin : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+
+    private lateinit var binding : ActivityOpcionesLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_opciones_login)
+        binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.IngresarEmail.setOnClickListener {
+            startActivity(Intent(this@OpcionesLogin, Login_email::class.java))
+        }
 
         val constraintLayout: RelativeLayout = findViewById(R.id.mainLayout)
         val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
---
+++ app/src/main/java/com/ivetteVG/ecommerce/Opciones_Login/Login_email.kt
@@ -0,0 +1,12 @@
+package com.ivetteVG.ecommerce.Opciones_Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivetteVG.ecommerce.R
+
+class Login_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_email)
+    }
+}
