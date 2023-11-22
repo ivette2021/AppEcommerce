5459c8720a8c2ba0673ad6a4a30c91d1f8020824
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-22 17:35:35 -0300

    leer datos del usuario

--- app/src/main/java/com/ivetteVG/ecommerce/Constantes.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/Constantes.kt
@@ -1,8 +1,20 @@
 package com.ivetteVG.ecommerce
 
+import android.text.format.DateFormat
+import java.util.Calendar
+import java.util.Locale
+
 object Constantes { //clase nos servira para crear diversas funciones
 
-    fun obtenerTiempoDis() : Long{
+    fun obtenerTiempoDis(): Long {
         return System.currentTimeMillis()//nos va a permitir saber el tiempo actual que se esta ejecutando
+
+    }
+
+    fun obtenerFecha(tiempo: Long):String {
+        val calendario = Calendar.getInstance(Locale.ENGLISH)
+        calendario.timeInMillis = tiempo
+
+        return DateFormat.format("DD/MM/YYYY", calendario).toString()
     }
 }
