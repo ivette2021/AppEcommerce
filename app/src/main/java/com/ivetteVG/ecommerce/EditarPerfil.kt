d7ce6dd6deb3546df2b5a23d0187c16cc99e1576
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-28 23:57:21 -0300

    -Actualizar imagen del usuario (sobreescribir en BD)

--- app/src/main/java/com/ivetteVG/ecommerce/EditarPerfil.kt
+++ app/src/main/java/com/ivetteVG/ecommerce/EditarPerfil.kt
@@ -19,6 +19,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.ivetteVG.ecommerce.databinding.ActivityEditarPerfilBinding
 
 class EditarPerfil : AppCompatActivity() {
@@ -26,7 +27,8 @@ class EditarPerfil : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var progressDialog: ProgressDialog
 
-    private var imageUri: Uri?= null //creamos esta variable tipo uri para almacenar y inicializamos como posible nulo
+    private var imageUri: Uri? =
+        null //creamos esta variable tipo uri para almacenar y inicializamos como posible nulo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,6 +96,50 @@ class EditarPerfil : AppCompatActivity() {
             })
     }
 
+    private fun subirImagenStorage() {
+        progressDialog.setMessage("Subiendo imagen a Storage")
+        progressDialog.show()
+
+        val rutaImagen =
+            "imagenesperfil/" + firebaseAuth.uid //crear una carpeta en firebase, con el uid vamos a identificar cada imagen del usuario y ahorramos espacio debido a que se sobreescribe
+        val ref = FirebaseStorage.getInstance().getReference(rutaImagen)
+        ref.putFile(imageUri!!)
+            .addOnSuccessListener { taskSnapShot ->
+                val uriTask =
+                    taskSnapShot.storage.downloadUrl //setear la variable de imagen que esta en storage
+                while (!uriTask.isSuccessful);
+                val urlImagenCargada = uriTask.result.toString()
+                if (uriTask.isSuccessful) {
+                    actualizarImagenBD(urlImagenCargada)
+                }
+            }.addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun actualizarImagenBD(urlImagenCargada: String) {
+        progressDialog.setMessage("Actualizando imagen")
+        progressDialog.show()
+
+        val hashMap : HashMap<String, Any> = HashMap()
+        if (imageUri != null){
+            hashMap["urlImagenPerfil"] = urlImagenCargada
+        }
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!)
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "Su imagen de perfil se ha actualizado", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e->
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
     private fun select_imagen_de() {
         val popupMenu = PopupMenu(this, binding.FABCambiarImg)
 
@@ -122,7 +168,7 @@ class EditarPerfil : AppCompatActivity() {
                     imagenGaleria() //si el usuario desea abrir galeria de un sdk igual o superior a tiramisu se ejecuta esta funcion
                 } else {
                     concederPermisoAlmacenamiento.launch(
-                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 
                     )
                 }
@@ -153,9 +199,13 @@ class EditarPerfil : AppCompatActivity() {
 
     private fun imageCamara() {
         val contentValues = ContentValues()
-        contentValues.put(MediaStore.Images.Media.TITLE, "Titulo_imagen")//sacar imagen con una alta calidad
+        contentValues.put(
+            MediaStore.Images.Media.TITLE,
+            "Titulo_imagen"
+        )//sacar imagen con una alta calidad
         contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion_Imagen")
-        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        imageUri =
+            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
 
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
@@ -163,19 +213,19 @@ class EditarPerfil : AppCompatActivity() {
     }
 
     private val resultadoCamara_ARL = //gestionar si la foto fue tomada correctamente
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            resultado ->
-            if (resultado.resultCode == Activity.RESULT_OK){
-                try {
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultado ->
+            if (resultado.resultCode == Activity.RESULT_OK) {
+                subirImagenStorage()
+                /*try {
                     Glide.with(this)
                         .load(imageUri)
                         .placeholder(R.drawable.img_perfil)
                         .into(binding.imgPerfil)
-                }catch (e:Exception){
+                } catch (e: Exception) {
 
-                }
-            }else{
-                Toast.makeText(this,"Cancelado",Toast.LENGTH_SHORT).show()
+                }*/
+            } else {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -195,26 +245,28 @@ class EditarPerfil : AppCompatActivity() {
 
     private fun imagenGaleria() {
         val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"//con esto decimos que cuando se habra la galeria solo tengamos a disposicion imagenes
+        intent.type =
+            "image/*"//con esto decimos que cuando se habra la galeria solo tengamos a disposicion imagenes
         resultadoGaleria_ARL.launch(intent)
     }
 
-    private  val resultadoGaleria_ARL =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){resultado->
-            if (resultado.resultCode ==Activity.RESULT_OK){ //si la imagen due seleccionada correctamente
+    private val resultadoGaleria_ARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultado ->
+            if (resultado.resultCode == Activity.RESULT_OK) { //si la imagen due seleccionada correctamente
                 val data = resultado.data
                 imageUri = data!!.data
+                subirImagenStorage()
                 //setear el imageview
-                try {
+                /*try {
                     Glide.with(this)
                         .load(imageUri)
                         .placeholder(R.drawable.img_perfil)
                         .into(binding.imgPerfil)
-                }catch (e:Exception){
+                } catch (e: Exception) {
 
-                }
-            }else{
-                Toast.makeText(this,"Cancelado",Toast.LENGTH_SHORT).show()
+                }*/
+            } else {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
             }
         }
 }
